80cb04687fd0f9860e33ec20331b3dea2113f6a3
aihki <laine.nestori.julius@gmail.com>
2024-03-18 19:16:28 +0200

    Add API hooks for user, authentication, and file operations

---
+++ src/hooks/apiHooks.ts
@@ -0,0 +1,96 @@
+import * as FileSystem from 'expo-file-system';
+import { useState, useEffect } from 'react';
+import {fetchData} from '../lib/utils';
+import {Credentials} from '../types/LocalTypes';
+import {
+  LoginResponse,
+  MessageResponse,
+  UploadResponse,
+  UserResponse,
+} from '../types/MessageTypes';
+import {useUpdateContext} from './UpdateHooks';
+
+
+const useUser = () => {
+const postUser = async (user: Record<string, string>)=> {
+  const options: RequestInit = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+  await fetchData<UserResponse>(
+    process.env.EXPO_PUBLIC_AUTH_API + '/users/',
+    options,
+  );
+};
+
+const getUsernameAvailability = async (username: string) => {
+return  fetchData<{available: boolean}>(
+    process.env.EXPO_PUBLIC_AUTH_API + '/users/username/' + username,
+  );
+};
+
+const getEmailAvailable = async (email: string) => {
+  return await fetchData<{available: boolean}>(
+    process.env.EXPO_PUBLIC_AUTH_API + '/users/email/' + email,
+  );
+};
+return {postUser, getUsernameAvailability, getEmailAvailable};
+};
+
+const useAuthentication = () => {
+  const postLogin = async (creds: Credentials) => {
+    return await fetchData<LoginResponse>(
+      process.env.EXPO_PUBLIC_AUTH_API + '/auth/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(creds),
+      },
+    );
+  };
+
+  return {postLogin};
+};
+
+
+const useFile = () => {
+  const postFile = async (file: File, token: string) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+      body: formData,
+    };
+    return await fetchData<UploadResponse>(
+      process.env.EXPO_PUBLIC_UPLOAD_API + '/upload',
+      options,
+    );
+  };
+
+  const postExpoFile = async (imageUri: string, token: string): Promise<UploadResponse> => {
+    const fileResult = await FileSystem.uploadAsync(
+      process.env.EXPO_PUBLIC_UPLOAD_API + '/upload',
+      imageUri,
+      {
+        httpMethod: 'POST',
+        uploadType: FileSystem.FileSystemUploadType.MULTIPART,
+        fieldName: 'file',
+        headers: {
+          Authorization: 'Bearer ' + token,
+        },
+      },
+    );
+    return JSON.parse(fileResult.body);
+  };
+  return {postFile, postExpoFile};
+};
+
+export {useUser, useAuthentication, useFile};
